acdc9fb7a7a247548ad83a55f699100ca549eef1
Ian Fox <ianfox41@gmail.com>
2016-02-01 08:34:38 +0000

    tested and finished hw0

--- hw-ruby-intro-master/lib/ruby_intro.rb
+++ hw-ruby-intro-master/lib/ruby_intro.rb
@@ -3,15 +3,22 @@
 # Part 1
 
 def sum arr
-  arr.inject { |sum, var|  sum + var}
+   ret = arr.inject { |sum, var|  sum + var}
+   return 0 if ret == nil
+   ret
 end
 
 def max_2_sum arr
-  arr.max(2)
+  sum arr.max(2)
 end
 
 def sum_to_n? arr, n
-  sum arr[0...n]
+  sum = Hash.new
+  for i in arr
+      return true if sum[i] != nil
+      sum[n - i] = i
+  end
+  false
 end
 
 # Part 2
@@ -21,18 +28,20 @@ def hello(name)
 end
 
 def starts_with_consonant? s
-  (s =~ /^[^AEIOU]/i) != nil
+  (s =~ /^[^AEIOU\W]/i) != nil
 end
 
 def binary_multiple_of_4? s
-	s =~ /([0+])?(1[01]*)/
-	return if s == nil
+	return false if s.length == 0
+	s =~ /([^01])/
+        return false if $1 != nil
+	s =~ /([0]+)?(1[01]*)?/
+	return true if ($2 == nil && $1 != nil)
 	s = $2  # remove zeros
 	i = 0
 	sum = 0
 	while i < s.length
 		sum = sum << 1
-		
 		sum += (s[i].to_i)
 		i += 1
 	end
@@ -75,4 +84,11 @@ class BookInStock
 			price_string = $1 + "." + $2[0...2]
 		end
 		"$" + price_string
+	end
 end
+
+
+
+
+
+
