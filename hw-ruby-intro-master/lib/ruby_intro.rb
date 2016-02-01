c80ab70a828364825e83094e6991e46518fa3b7a
Ian Fox <ianfox41@gmail.com>
2016-01-31 17:20:03 -0800

    initial commit

---
+++ hw-ruby-intro-master/lib/ruby_intro.rb
@@ -0,0 +1,78 @@
+# When done, submit this entire file to the autograder.
+
+# Part 1
+
+def sum arr
+  arr.inject { |sum, var|  sum + var}
+end
+
+def max_2_sum arr
+  arr.max(2)
+end
+
+def sum_to_n? arr, n
+  sum arr[0...n]
+end
+
+# Part 2
+
+def hello(name)
+  "Hello, " + name
+end
+
+def starts_with_consonant? s
+  (s =~ /^[^AEIOU]/i) != nil
+end
+
+def binary_multiple_of_4? s
+	s =~ /([0+])?(1[01]*)/
+	return if s == nil
+	s = $2  # remove zeros
+	i = 0
+	sum = 0
+	while i < s.length
+		sum = sum << 1
+		
+		sum += (s[i].to_i)
+		i += 1
+	end
+	sum % 4 == 0
+end
+
+# Part 3
+
+class BookInStock
+	def initialize(isbn, price)
+		raise(ArgumentError, "isbn must be non empty") unless isbn.length > 0
+		raise(ArgumentError, "price must be greater than 0") unless price > 0
+		@isbn = isbn
+		@price = price
+	end
+
+	def price
+		@price
+	end
+
+	def isbn
+		@isbn
+	end
+
+	def price=(new_price)
+		@price = new_price
+	end
+
+	def isbn=(new_isbn)
+		@isbn = new_isbn
+	end
+
+	def price_as_string
+		price_string = @price.to_f.to_s
+		price_string =~ /(\d+).(\d+)/
+		if $2.length < 2
+			price_string += "0"
+		end
+		if $2.length > 2
+			price_string = $1 + "." + $2[0...2]
+		end
+		"$" + price_string
+end
